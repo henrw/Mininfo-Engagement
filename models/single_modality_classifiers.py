8ab4c961cfd01a6a6478a8ea27e28101bcefd29a
Muzhe Wu <henrw@lit1000.eecs.umich.edu>
2022-10-10 21:30:21 -0400

    Train wav2vec, concate model

--- models/single_modality_classifiers.py
+++ models/single_modality_classifiers.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch
-from transformers import RobertaModel, RobertaTokenizer
+from transformers import RobertaModel, RobertaTokenizer, BertTokenizer, BertModel
 from .base import *
 
 class RobertaClassifier(nn.Module):
@@ -15,6 +15,38 @@ class RobertaClassifier(nn.Module):
     def reset(self):
         self.tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
         self.base = RobertaModel.from_pretrained('roberta-base')
+        self.linear1 = nn.Linear(768,10)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(10,self.num_classes)
+
+    def forward(self, token):
+        if isinstance(token,str):
+            token = self.tokenizer(token, return_tensors="pt")
+        elif isinstance(token, list):
+            token = self.tokenizer(token, return_tensors="pt", max_length = 512, padding=True, truncation=True)
+        output = self.base(**token)
+        output = self.linear1(output['pooler_output'])
+        output = self.relu(output)
+        output = self.linear2(output)
+
+        return output
+    
+    def predict(self, token):
+      digits = self.forward(token)
+      return digits.argmax()
+
+class BertClassifier(nn.Module):
+
+    def __init__(self, num_classes) -> None:
+        super().__init__()
+        self.num_classes = num_classes
+        self.reset()
+        test_message = "Initialization success."
+        print(f"Initialization success if you see a tensor: {self.forward(test_message)}.")
+    
+    def reset(self):
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        self.base = BertModel.from_pretrained("bert-base-uncased")
         self.linear1 = nn.Linear(768,10)
         self.relu = nn.ReLU()
         self.linear2 = nn.Linear(10,self.num_classes)
